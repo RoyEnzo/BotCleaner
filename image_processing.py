45ea9394067d2cdab7f06b49557a4295b71b89c9
enzo <enzo.roy@hotmail.fr>
2019-05-14 16:48:18 +0200

    analyse image

--- image_processing.py
+++ image_processing.py
@@ -7,8 +7,18 @@ import numpy as np
 URL = 'http://10.134.97.49:5000/camera/last_frame'
 
 image_bgr = io.imread(URL)
-image_hsv = cv2.cvtColor(image_bgr, cv2.COLOR_RGB2HSV)
-io.imshow(image_hsv)
+
+image_hsv = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2HSV)
+
+# rouge-orange
+lower_orange = np.array([100, 100, 100])  #RGB ???? (fait par tatonement)
+upper_orange = np.array([130, 255, 255])
+
+mask = cv2.inRange(image_hsv, lower_orange, upper_orange)
+
+io.imshow(mask)
+#io.imshow(image_bgr)
+
 plt.show()
 
 
